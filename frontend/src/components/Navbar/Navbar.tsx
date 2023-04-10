7a8ddbac0bddea5d340ac8d021231e10564c237e
adam3norton <adam3norton@gmail.com>
2023-04-10 12:37:24 -0600

    Frontend Skeleton

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <section className="Navbar">
+            <Link to="/">
+                <div className="NavButton">Logo</div>
+            </Link>
+            <Link to="/">
+                <div className="NavButton btn">Home</div>
+            </Link>
+            <Link to="/summary">
+                <div className="NavButton btn">Summary</div>
+            </Link>
+            <Link to="/supervised">
+                <div className="NavButton btn">Supervised</div>
+            </Link>
+            <Link to="/unsupervised">
+                <div className="NavButton btn">Unsupervised</div>
+            </Link>
+            <Link to="/privacy">
+                <div className="NavButton btn">Privacy</div>
+            </Link>
+            <Link to="/login">
+                <div className="NavButton btn">Login</div>
+            </Link>
+            <Link to="/login">
+                <div className="NavButton CreateAccount" style={{ color: "white" }}>Create Account</div>
+            </Link>
+        </section>
+    )
+}
+
+export default Navbar;
