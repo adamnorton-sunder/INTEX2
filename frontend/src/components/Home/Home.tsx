0a26373232e9b36bbdb37717759206b5981ffcfb
annadalley <annadalley106@gmail.com>
2023-04-12 12:02:29 -0600

    Home Page Information

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -35,7 +35,52 @@ function Home() {
           <Swiper items={items} />
         </div>
         <div style={{ maxWidth: "1100px" }}>
-          <p>This is the cool homepage.</p>
+          <h2>Fag El-Gamous</h2>
+          <p>
+            For over 25 years, Fag el-Gamous has been under archeological
+            investigation. Many insights have been gathered from studying this
+            ancient cemetery. Researchers have excavated more than 1,000 mummies
+            from this sight and gathered information from location, sex, age,
+            hair color, bone dimensions, textiles, wrapping, burial depth,
+            physical orientation in ground, and others.
+          </p>
+          <p>
+            This website aims to create an interactive experience for interested
+            users to view the data, and see some of the analytics that have been
+            run against the data. Researchers are continually adding new data
+            from the excavation, so users can visit the page often to view these
+            incredible findings. Our Supervised and Unsupervised pages aim to
+            predict, explain and visualize the wealth of data recieved, to help
+            Researchers gain further insights.
+          </p>
+          <p>
+            Ready to see the data?&nbsp;
+            <a href="/summary" className="article-link">
+              Click here
+            </a>
+          </p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <p>
+            Want to learn more about Fag El-Gamous and the academic findings?
+            Click the link below to read about the research done on Burial Dates
+          </p>
+          <p>
+            <a
+              href="https://www.sciencedirect.com/science/article/pii/S2352409X15000188"
+              className="article-link"
+            >
+              Rethinking burial dates at a Graeco-Roman Cemetery: Fag el-Gamous,
+              Fayoum, Egypt
+            </a>
+            &nbsp;by R. Paul Evans, David M. Whitchurch, Kerry Muhlestein
+          </p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
         </div>
         <CookieConsent
           location="bottom"
@@ -46,9 +91,7 @@ function Home() {
           expires={0}
         >
           This website uses cookies to enhance the user experience.{" "}
-          <span style={{ fontSize: "10px" }}>
-            This bit of text is smaller :O
-          </span>
+          <span style={{ fontSize: "10px" }}></span>
         </CookieConsent>
       </div>
       <footer style={{ display: "grid", placeItems: "center" }}>
