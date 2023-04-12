5ec3daf9703541fe166de7f18d9b1c5f12b45075
Dallin Blanc <dallinb94@gmail.com>
2023-04-12 15:51:13 -0600

    added an egyptcontroller to serve up the json data

---
+++ backend/INTEX2/Controllers/EgyptController.cs
@@ -0,0 +1,24 @@
+﻿using INTEX2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace INTEX2.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EgyptController : ControllerBase
+    {
+        private postgresContext _context;
+
+        public EgyptController(postgresContext temp)
+        {
+            _context = temp;
+        }
+
+        public IEnumerable<Textile> Get()
+        {
+            return _context.Textiles
+                .ToArray();
+        }
+    }
+}
