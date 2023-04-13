066a449dcfcdbb88d1a7fe8c52757a2fd964d9c7
Dallin Blanc <dallinb94@gmail.com>
2023-04-13 13:17:53 -0600

    api in production mode

--- backend/INTEX2/Controllers/EgyptController.cs
+++ backend/INTEX2/Controllers/EgyptController.cs
@@ -15,10 +15,67 @@ namespace INTEX2.Controllers
             _context = temp;
         }
 
-        public IEnumerable<Textile> Get()
+        [HttpGet]
+        public IActionResult Get()
         {
-            return _context.Textiles
-                .ToArray();
+            var records = from burial in _context.Burialmains
+                          join burialTextile in _context.BurialmainTextiles on burial.Id equals burialTextile.MainBurialmainid
+                          join textile in _context.Textiles on burialTextile.MainTextileid equals textile.Id
+                          join colorTextile in _context.ColorTextiles on textile.Id equals colorTextile.MainTextileid
+                          join color in _context.Colors on colorTextile.MainColorid equals color.Id
+                          join tt in _context.TextilefunctionTextiles on textile.Id equals tt.MainTextileid
+                          join tf in _context.Textilefunctions on tt.MainTextilefunctionid equals tf.Id
+                          select new
+                          {
+                              burial.Id,
+                              burial.Squarenorthsouth,
+                              burial.Headdirection,
+                              burial.Sex,
+                              burial.Northsouth,
+                              burial.Depth,
+                              burial.Eastwest,
+                              burial.Adultsubadult,
+                              burial.Facebundles,
+                              burial.Southtohead,
+                              burial.Preservation,
+                              burial.Fieldbookpage,
+                              burial.Squareeastwest,
+                              burial.Goods,
+                              burial.Text,
+                              burial.Wrapping,
+                              burial.Haircolor,
+                              burial.Westtohead,
+                              burial.Samplescollected,
+                              burial.Area,
+                              burial.Burialid,
+                              burial.Length,
+                              burial.Burialnumber,
+                              burial.Dataexpertinitials,
+                              burial.Westtofeet,
+                              burial.Ageatdeath,
+                              burial.Southtofeet,
+                              burial.Excavationrecorder,
+                              burial.Photos,
+                              burial.Hair,
+                              burial.Burialmaterials,
+                              burial.Dateofexcavation,
+                              burial.Fieldbookexcavationyear,
+                              burial.Clusternumber,
+                              burial.Shaftnumber,
+                              textile.Locale,
+                              textile.Textileid,
+                              textile.Description,
+                              textile.Estimatedperiod,
+                              textile.Sampledate,
+                              textile.Photographeddate,
+                              textile.Direction,
+                              color.Value,
+                              color.Colorid,
+                              //tf.Value,
+                              tf.Textilefunctionid
+                          };
+
+            return Ok(records);
         }
     }
 }
